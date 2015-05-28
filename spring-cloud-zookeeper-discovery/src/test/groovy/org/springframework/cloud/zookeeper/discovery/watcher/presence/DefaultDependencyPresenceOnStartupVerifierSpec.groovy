891d4ec76cd94fbcd922810c36f794c9340baa8f
Marcin Grzejszczak <marcin.grzejszczak@4finance.com>
2015-05-28 16:46:45 +0200

    Added copyright headers

--- spring-cloud-zookeeper-discovery/src/test/groovy/org/springframework/cloud/zookeeper/discovery/watcher/presence/DefaultDependencyPresenceOnStartupVerifierSpec.groovy
+++ spring-cloud-zookeeper-discovery/src/test/groovy/org/springframework/cloud/zookeeper/discovery/watcher/presence/DefaultDependencyPresenceOnStartupVerifierSpec.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.cloud.zookeeper.discovery.watcher.presence
 
 import org.apache.curator.x.discovery.ServiceCache
@@ -10,14 +25,14 @@ class DefaultDependencyPresenceOnStartupVerifierSpec extends Specification {
 
 	def 'should throw exception if obligatory dependencies are missing'() {
 		given:
-		DefaultDependencyPresenceOnStartupVerifier dependencyVerifier = new DefaultDependencyPresenceOnStartupVerifier()
-		ServiceCache serviceCache = Mock()
-		serviceCache.instances >> []
+			DefaultDependencyPresenceOnStartupVerifier dependencyVerifier = new DefaultDependencyPresenceOnStartupVerifier()
+			ServiceCache serviceCache = Mock()
+			serviceCache.instances >> []
 		when:
-		dependencyVerifier.verifyDependencyPresence(SERVICE_NAME, serviceCache, true)
+			dependencyVerifier.verifyDependencyPresence(SERVICE_NAME, serviceCache, true)
 		then:
-		Throwable thrown = thrown(Throwable)
-		StackTraceUtils.extractRootCause(thrown).class == NoInstancesRunningException
+			Throwable thrown = thrown(Throwable)
+			StackTraceUtils.extractRootCause(thrown).class == NoInstancesRunningException
 	}
 
 }
